b7b8df15166c5a4fca11b75aee290c2cfe0f2dfd
Mario Fernandez <diddleslip@gmail.com>
2020-09-30 11:31:14 -0400

    Changed not equal operator to equal

--- server/src/constants.ts
+++ server/src/constants.ts
@@ -1,1 +1,1 @@
-export const __prod__ = process.env.NODE_ENV !== "production"; // Used to activate debug on mikro-orm.config only if in development
+export const __prod__ = process.env.NODE_ENV === "production"; // Used to activate debug on mikro-orm.config only if in development
